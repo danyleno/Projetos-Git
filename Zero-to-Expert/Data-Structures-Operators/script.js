146b602a218c88f58c29390d931f845288b73193
Daniel Botelho Souza <100227552+danyleno@users.noreply.github.com>
2023-09-30 09:41:03 -0300

    maps

--- Zero-to-Expert/Data-Structures-Operators/script.js
+++ Zero-to-Expert/Data-Structures-Operators/script.js
@@ -52,6 +52,62 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+
+// Maps
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'], 
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct'],
+  [false, 'Try again!']
+])
+// Convert object to map
+console.log(Object.entries(openingHours))
+const objectToMap = new Map (Object.entries(openingHours))
+console.log(objectToMap)
+
+// Quiz app
+console.log(question.get('question'))
+for (const [key, value] of question){
+  if(typeof key === 'number')console.log(`Answer ${key}: ${value}`)   
+} 
+const answer = 3 //Number(prompt('Your Answer'))
+console.log(question.get(question.get('correct') === answer))
+
+// Convert map to array
+console.log(question)
+console.log([...question])
+
+/*const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisboa, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 24)
+  .set(true, 'We are open :)')
+  .set(false, 'We are closed :(');
+  console.log(rest.get('name'))
+  console.log(rest.get(false))
+  console.log(rest.get(1))
+
+  const time = 19
+  console.log(rest.get(time > rest.get('open') && time < rest.get('close')))
+  rest.delete(2)
+  //rest.clear()
+  console.log(rest)
+  console.log(rest.size)
+  // Atribuíndo array como key
+  const arr = [1, 2]
+  rest.set(arr, 'Test')
+  console.log(rest.get(arr))
+  // Atribuindo um h1 com key
+  console.log(rest.set(document.querySelector('h1'), 'Cabeçalho'))
+/*
 // Sets
 const ordersSet = new Set([
   'Pasta',
